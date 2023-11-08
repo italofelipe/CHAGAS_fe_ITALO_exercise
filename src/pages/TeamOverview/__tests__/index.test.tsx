ceb997652e69ddf5dd61a380bb6d5e8197c383de
italochagas <felipe.italo@hotmail.com>
2023-11-08 09:17:04 -0300

    test: added tests for input filter

--- src/pages/TeamOverview/__tests__/index.test.tsx
+++ src/pages/TeamOverview/__tests__/index.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {render, screen, waitFor} from '@testing-library/react';
+import {fireEvent, render, screen, waitFor} from '@testing-library/react';
 import * as API from '../../../api';
 import TeamOverview from '..';
 
@@ -29,35 +29,36 @@ describe('TeamOverview', () => {
     afterAll(() => {
         jest.useRealTimers();
     });
+    const teamMembers = [
+        {
+            id: '1',
+            firstName: 'John',
+            lastName: 'Doe',
+            displayName: 'JohnDoe',
+            location: 'New York',
+            avatar: '',
+        },
+        {
+            id: '2',
+            firstName: 'Jane',
+            lastName: 'Smith',
+            displayName: 'JaneSmith',
+            location: 'London',
+            avatar: '',
+        },
+        {
+            id: '3',
+            firstName: 'Bob',
+            lastName: 'Johnson',
+            displayName: 'BobJohnson',
+            location: 'Paris',
+            avatar: '',
+        },
+    ];
 
+    const teamOverview = {teamLeadId: '1', teamMemberIds: ['2', '3']};
     it('should render team overview users', async () => {
-        const teamOverview = {teamLeadId: '1', teamMemberIds: ['2', '3']};
-        const teamMembers = [
-            {
-                id: '1',
-                firstName: 'John',
-                lastName: 'Doe',
-                displayName: 'JohnDoe',
-                location: 'New York',
-                avatar: '',
-            },
-            {
-                id: '2',
-                firstName: 'Jane',
-                lastName: 'Smith',
-                displayName: 'JaneSmith',
-                location: 'London',
-                avatar: '',
-            },
-            {
-                id: '3',
-                firstName: 'Bob',
-                lastName: 'Johnson',
-                displayName: 'BobJohnson',
-                location: 'Paris',
-                avatar: '',
-            },
-        ];
+        
         jest.spyOn(API, 'getTeamOverview').mockResolvedValue(teamOverview);
         jest.spyOn(API, 'getUserData').mockImplementation(userId => {
             const user = teamMembers.find(member => member.id === userId);
@@ -67,5 +68,39 @@ describe('TeamOverview', () => {
         await waitFor(() => {
             expect(screen.getByText('John Doe')).toBeInTheDocument();
         });
+    });
+
+    it('should filter team member based on input value', async () => {
+        jest.spyOn(API, 'getTeamOverview').mockResolvedValue(teamOverview);
+        jest.spyOn(API, 'getUserData').mockImplementation(userId => {
+            const user = teamMembers.find(member => member.id === userId);
+            return Promise.resolve(user);
+        });
+        render(<TeamOverview />);
+
+        await waitFor(() => {
+            expect(screen.getByText('JaneSmith')).toBeInTheDocument();
+        });
+
+        await waitFor(() => {
+            expect(screen.getByText('BobJohnson')).toBeInTheDocument();
+        });
+
+        await waitFor(() => {
+            expect(screen.getByText('JohnDoe')).toBeInTheDocument();
+        });
+
+        const inputElement = screen.getByPlaceholderText('Filter by display name');
+        fireEvent.change(inputElement, {target: {value: 'JaneSmith'}});
+        await waitFor(() => {
+            expect(screen.getByText('JaneSmith')).toBeInTheDocument();
+        });
+        await waitFor(() => {
+            expect(screen.queryByText('JohnDoe')).toBeInTheDocument();
+        });
+        await waitFor(() => {
+            expect(screen.queryByText('BobJohnson')).toBeNull();
+        });
+        
     });
 });
--- src/pages/Teams/__tests__/index.test.tsx
+++ src/pages/Teams/__tests__/index.test.tsx
@@ -15,6 +15,8 @@ jest.mock('react-router-dom', () => ({
     useNavigate: () => ({}),
 }));
 
+jest.mock('../../../api');
+
 describe('Teams', () => {
     beforeAll(() => {
         jest.useFakeTimers();
@@ -53,5 +55,54 @@ describe('Teams', () => {
             expect(screen.getByText('Team1')).toBeInTheDocument();
         });
         expect(screen.getByText('Team2')).toBeInTheDocument();
+    });
+
+    it('should render an input field with a placeholder "Filter by display name"', () => {
+        render(<Teams />);
+        const inputElement = screen.getByPlaceholderText('Filter by display name');
+        expect(inputElement).toBeInTheDocument();
+    });
+
+    it('should filter teams based on input value', async () => {
+        jest.spyOn(API, 'getTeams').mockResolvedValue([
+            {
+                id: '1',
+                name: 'Barcelona',
+            },
+            {
+                id: '2',
+                name: 'Real Madrid',
+            },
+            {
+                id: '3',
+                name: 'Manchester United',
+            },
+        ]);
+        render(<Teams />);
+
+        await waitFor(() => {
+            expect(screen.getByText('Barcelona')).toBeInTheDocument();
+        });
+
+        await waitFor(() => {
+            expect(screen.getByText('Real Madrid')).toBeInTheDocument();
+        });
+
+        await waitFor(() => {
+            expect(screen.getByText('Manchester United')).toBeInTheDocument();
+        });
+
+        const inputElement = screen.getByPlaceholderText('Filter by display name');
+        fireEvent.change(inputElement, {target: {value: 'Barcelona'}});
+
+        await waitFor(() => {
+            expect(screen.queryByText('Barcelona')).toBeInTheDocument();
+        });
+        await waitFor(() => {
+            expect(screen.queryByText('Real Madrid')).toBeNull();
+        });
+        await waitFor(() => {
+            expect(screen.queryByText('Manchester United')).toBeNull();
+        });
     });
 });
